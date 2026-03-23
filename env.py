69a09633f23086711f25cd7ba06aa799cce1f7a6
Gruheem <grahamsilcock91@gmail.com>
2026-03-23 14:40:50 +0000

    change .gitignore

--- env.py
+++ env.py
@@ -1,3 +1,4 @@
 import os
 
 os.environ.setdefault('DATABASE_URL', 'postgresql://neondb_owner:npg_57PhuTzagrqQ@ep-falling-truth-altulr1j.c-3.eu-central-1.aws.neon.tech/grid_stick_racer_219459')
+os.environ.setdefault("SECRET_KEY", "django-insecure-7k7zlzzk@h=_e(00cizn)i(_avlh&+7ma8d25u0+&fw1)$u%o5")
