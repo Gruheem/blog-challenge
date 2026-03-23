81cb98a3b58618c91c41f0d9089f05582965c920
Gruheem <grahamsilcock91@gmail.com>
2026-03-23 12:46:18 +0000

    Make Allowed Hosts '*'

--- codestar/settings.py
+++ codestar/settings.py
@@ -26,11 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'dev-secret-key')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [
-    'blog-excercise-913a145fa962.herokuapp.com/',
-    '127.0.0.1',
-    'localhost',
-]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
