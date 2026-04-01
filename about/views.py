9e84ea1e4e712a7cdbb8d9bb9771b4c9ecd7829a
Gruheem <grahamsilcock91@gmail.com>
2026-04-01 15:49:57 +0100

    Add Comment CRUD

--- about/views.py
+++ about/views.py
@@ -1,5 +1,7 @@
+from django.contrib import messages
 from django.shortcuts import render
 from .models import About
+from .forms import CollaborateForm
 
 # Create your views here.
 def about_page(request):
@@ -7,11 +9,21 @@ def about_page(request):
     Display the about page.
 
     """
-    about = About.objects.first()
+    if request.method == "POST":
+        collaborate_form = CollaborateForm(data=request.POST)
+        if collaborate_form.is_valid():
+            collaborate_form.save()
+            messages.add_message(request, messages.SUCCESS, 'Collaboration request submitted successfully, I will endevour to respond within two days.')
+            
+    about = About.objects.all().order_by('-updated_on').first()
+    collaborate_form = CollaborateForm()
+
     return render(
         request,
         "about/about.html",
-        {"about": about,
+        {
+            "about": about,
+            "collaborate_form": collaborate_form,
         },
     )
     
